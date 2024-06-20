7393690cfa3a2c0f613c5f8564a37fbf349bc8f5
MUHAMMAD RAIHAN <muhammadraihan@mail.ugm.ac.id>
2024-06-20 20:39:24 +0700

    Deploy Client

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -8,7 +10,7 @@ const port = 3000;
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
   },
 });
 
