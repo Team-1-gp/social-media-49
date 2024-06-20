896c55a4870dec4e82da51be2a5b530fb804f92e
MUHAMMAD RAIHAN <muhammadraihan@mail.ugm.ac.id>
2024-06-20 20:26:39 +0700

    feat(client): Styling Home Page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,8 @@ import { useContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import AppContext from "../context/AppContext";
 import { useNavigate } from "react-router-dom";
+import { GoComment, GoDotFill } from "react-icons/go";
+import { FcLike, FcLikePlaceholder } from "react-icons/fc";
 
 const socket = io("http://localhost:3000");
 
@@ -9,6 +11,7 @@ export default function HomePage() {
   const { user, setUser, posts, setPosts } = useContext(AppContext);
   const [newPost, setNewPost] = useState("");
   const [comment, setComment] = useState("");
+  const [likedPosts, setLikedPosts] = useState([]);
   const [userOnline, setUserOnline] = useState([]);
   const [selectedPost, setSelectedPost] = useState(null);
 
@@ -60,7 +63,8 @@ export default function HomePage() {
     };
   }, []);
 
-  const handlePostSubmit = () => {
+  const handlePostSubmit = (e) => {
+    e.preventDefault();
     const post = {
       id: Date.now(),
       username: user.username,
@@ -83,7 +87,15 @@ export default function HomePage() {
 
   const handleLike = (postId) => {
     const updatedPost = posts.find((post) => post.id === postId);
-    updatedPost.likes += 1;
+    const isLiked = likedPosts.includes(postId);
+
+    if (!isLiked) {
+      updatedPost.likes += 1;
+      setLikedPosts([...likedPosts, postId]);
+    } else {
+      updatedPost.likes -= 1;
+      setLikedPosts(likedPosts.filter((id) => id !== postId));
+    }
     setPosts([...posts]);
     socket.emit("like", updatedPost);
   };
@@ -91,73 +103,108 @@ export default function HomePage() {
   console.log(userOnline);
 
   return (
-    <div className="px-40 py-10">
-      {userOnline?.map((u) => (
-        <h1 key={u.id}>{u.username}</h1>
-      ))}
-      <div className="text-end">
-        <button
-          onClick={() => {
-            localStorage.clear();
-            navigate("/login");
-          }}
-        >
-          Logout
-        </button>
-      </div>
-      <h1 className="text-center text-4xl mb-4">Welcome, {user.username}</h1>
-      <div className="mb-4">
-        <textarea
-          className="px-4 py-2 mb-4 border border-black w-full"
-          value={newPost}
-          onChange={(e) => setNewPost(e.target.value)}
-          placeholder="What's on your mind?"
-        />
-        <div className="flex justify-end">
-          <button
-            className="px-4 py-2 border border-black font-bold rounded-2xl"
-            onClick={handlePostSubmit}
-          >
-            Post
-          </button>
-        </div>
-      </div>
-      <div>
-        {posts?.map((post) => (
-          <div key={post.id} className="border border-black p-4 mb-4">
-            <h3 className="font-semibold text-xl">{post.username}</h3>
-            <p>{post.content}</p>
-            <button className="mr-4" onClick={() => handleLike(post.id)}>
-              Like ({post.likes})
-            </button>
-            <button onClick={() => setSelectedPost(post.id)}>Comment</button>
-            {selectedPost === post.id && (
-              <div>
-                <textarea
-                  className="px-4 py-2 my-4 border border-black w-full"
-                  value={comment}
-                  onChange={(e) => setComment(e.target.value)}
-                  placeholder="Write a comment..."
-                />
-                <div className="flex justify-end">
-                  <button
-                    className="px-4 py-2 border border-black font-bold rounded-2xl"
-                    onClick={() => handleCommentSubmit(post.id)}
-                  >
-                    Submit
-                  </button>
-                </div>
-              </div>
-            )}
-            <div className="mt-3">
-              {post.comments?.map((comment, index) => (
-                <p key={index}>
-                  <strong>{comment.username}</strong>: {comment.content}
-                </p>
-              ))}
-            </div>
+    <div className="flex min-h-screen">
+      <div className="p-5 bg-primary text-white">
+        <h1 className="text-3xl mb-4">Users Online</h1>
+        {userOnline?.map((u) => (
+          <div key={u.id} className="flex items-center">
+            <GoDotFill color="blue" />
+            <h1 key={u.id} className="text-xl">
+              {u.username}
+            </h1>
           </div>
         ))}
+      </div>
+      <div className="flex-1 p-5">
+        <div className="text-end">
+          <button
+            onClick={() => {
+              localStorage.clear();
+              navigate("/login");
+            }}
+          >
+            Logout
+          </button>
+        </div>
+        <h1 className="text-center text-4xl mb-4">
+          Welcome,{" "}
+          <span className="text-primary font-bold">{user.username}</span>
+        </h1>
+        <div className="mb-4">
+          <form onSubmit={handlePostSubmit}>
+            <textarea
+              required
+              className="resize-none px-4 py-2 mb-4 border border-primary rounded-lg w-full"
+              value={newPost}
+              onChange={(e) => setNewPost(e.target.value)}
+              placeholder="What's on your mind?"
+            />
+            <div className="flex justify-end">
+              <button
+                type="submit"
+                className="px-4 py-2 border bg-primary text-white border-primary rounded-lg font-bold "
+              >
+                Post
+              </button>
+            </div>
+          </form>
+        </div>
+        <div>
+          {posts?.map((post) => (
+            <div
+              key={post.id}
+              className="border border-primary rounded-lg p-4 mb-4"
+            >
+              <h3 className="font-semibold text-xl">{post.username}</h3>
+              <p>{post.content}</p>
+              <div className="flex">
+                <button
+                  className="mr-4 flex items-center gap-2"
+                  onClick={() => handleLike(post.id)}
+                >
+                  {likedPosts.includes(post.id) ? (
+                    <FcLike />
+                  ) : (
+                    <FcLikePlaceholder />
+                  )}
+                  {post.likes}
+                </button>
+                <button
+                  className="flex items-center gap-2"
+                  onClick={() => setSelectedPost(post.id)}
+                >
+                  <GoComment /> {post.comments.length}
+                </button>
+              </div>
+
+              {selectedPost === post.id && (
+                <div>
+                  <textarea
+                    className="px-4 py-2 my-4 border border-primary rounded-lg w-full"
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                    placeholder="Write a comment..."
+                  />
+                  <div className="flex justify-end">
+                    <button
+                      className="px-4 py-2 border bg-primary text-white border-primary font-bold rounded-lg"
+                      onClick={() => handleCommentSubmit(post.id)}
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              )}
+              <div className="mt-3">
+                {post.comments?.map((comment, index) => (
+                  <p key={index}>
+                    <strong>{comment.username}</strong>: {comment.content}
+                  </p>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -18,10 +18,12 @@ export default function LoginPage() {
     <div className="h-screen bg-[url('/cover.jpeg')] bg-contain flex flex-col justify-center items-center">
       {/* <img src="/cover.jpeg" alt="" /> */}
       <div className="bg-white p-10 rounded-lg">
-        <h1 className="text-4xl font-bold mb-8 text-center">Login</h1>
+        <h1 className="text-4xl font-bold mb-8 text-center text-primary">
+          Login
+        </h1>
         <form onSubmit={handleLogin}>
           <input
-            className="px-4 py-2 mb-4 w-full border border-black rounded-lg"
+            className="px-4 py-2 mb-4 w-full border border-primary active:border-primary rounded-lg"
             type="text"
             placeholder="Username"
             value={username}
@@ -30,7 +32,7 @@ export default function LoginPage() {
           />
           <div className="text-center">
             <button
-              className="px-4 py-2 hover:bg-primary hover:text-white border border-black font-bold rounded-lg"
+              className="px-4 py-2 text-primary hover:bg-primary hover:text-white border border-primary font-bold rounded-lg"
               type="submit"
             >
               Submit
